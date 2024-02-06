d64a734608354ef09c4093cfede14261f3845c96
Gibermar Regalado <gberxx@gmail.com>
2024-02-06 20:49:27 +0800

    Main Commit

--- src/components/floating-container/MainComponent.jsx
+++ src/components/floating-container/MainComponent.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import '../floating-container/main.scss'
+import sun from '../../assets/icon-sun.svg'
+import moon from '../../assets/icon-moon.svg'
+import DataContext from '../context/DataContext'
 
 const MainComponent = () => {
+
+  const { theme , handleToggle } = useContext(DataContext)
+
   return (
     <div className='main-container'>
-      
+      <div className="logo">
+        <h1>TODO</h1>
+        <button onClick={() => handleToggle()}><img src={theme ? sun : moon} alt="Icon" /></button>
+      </div>
     </div>
   )
 }
